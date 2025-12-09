1b029dd937ff86efdc4f62f551d8b754b72bf37c
Hansh <hansh0621@naver.com>
2025-12-09 15:47:49 +0900

    스프링 누락 파일 커밋

--- spring/TEAM202507-01/src/main/java/com/example/TEAM202507_01/menus/news/dto/NewsDto.java
+++ spring/TEAM202507-01/src/main/java/com/example/TEAM202507_01/menus/news/dto/NewsDto.java
@@ -2,8 +2,6 @@ package com.example.TEAM202507_01.menus.news.dto;
 
 import lombok.Data;
 
-import lombok.Data;
-
 @Data
 public class NewsDto {
 
--- spring/TEAM202507-01/src/main/java/com/example/TEAM202507_01/search/dto/SearchDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.TEAM202507_01.search.dto;
-
-import lombok.Data;
-
-@Data
-public class SearchDto {
-
-}
